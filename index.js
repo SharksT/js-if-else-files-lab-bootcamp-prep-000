56a1da6c0ff9f0a71045f6e4f573892af946bfde
Felipe Passos <felipekaires@hotmail.com>
2017-10-16 21:50:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if (name === "Susan") {
     console.log("The name is correct")
 }
 else {
-  console.log("Expect the name tu be Susan")
+  console.log("Expect the name to be Susan")
 }
 // Don't worry about this
 module.exports = { name, height
