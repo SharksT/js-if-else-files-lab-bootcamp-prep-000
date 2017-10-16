a8779dad51e6551e7089733947fc0902fe4813b5
Felipe Passos <felipekaires@hotmail.com>
2017-10-16 21:50:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var height = "74"
 if (name === "Susan") {
     console.log("The name is correct")
 }
+else {
+  console.log("Expect the name tu be Susan");
+}
 // Don't worry about this
 module.exports = { name, height
 }
-
